0cc2940fc6a0dd5c64d12dcd93fe050df4fb4d71
Waldo Strydom <88497072+Waldo-Strydom@users.noreply.github.com>
2025-06-27 17:37:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-// self.addEventListener("install", e =>{
-//      console.log("Hioijj")
-//     e.waitUntil(
-//         caches.open("static").then(cache =>{
-//             console.log("Hi")
-//             return cache.addAll(["./index.html", "./style.css", "./index.js", "./icons/apple-icon-180.png"])
-//         })
-//     );
-// })
+self.addEventListener("install", e =>{
+     console.log("Hioijj")
+    e.waitUntil(
+        caches.open("static").then(cache =>{
+            console.log("Hi")
+            return cache.addAll(["./index.html", "./style.css", "./index.js", "./icons/apple-icon-180.png"])
+        })
+    );
+})
 
-// self.addEventListener("fetch", e=>{
-//     console.log(`Intercepting for: ${e.request.url}`)
+self.addEventListener("fetch", e=>{
+    console.log(`Intercepting for: ${e.request.url}`)
 
-//     e.respondWith(
-//         caches.match(e.request).then(response=>{
-//             return response || fetch(e.request);
-//         })
-//     )
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    )
 
-// })
+})
